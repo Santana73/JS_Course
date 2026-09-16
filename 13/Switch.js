9052dff7c7ae1e269d06968c2689b95f2bf5f8e4
Santana73 <daniel.s.araujo7@ba.estudante.senai.br>
2026-09-16 10:27:44 -0300

    Aula 13
    Estudo Switch case

---
+++ 13/Switch.js
@@ -0,0 +1,30 @@
+let day = 0;
+
+switch(day){
+    case 1 :
+        console.log('Hj é Domingo');
+        break
+    case 2 :
+        console.log('Hj é Segunda');
+        break
+    case 3 :
+        console.log('Hj é Terça');
+        break
+    case 4 :
+        console.log('Hj é Quarta');
+        break
+    case 5 :
+        console.log('Hj é Quinta');
+        break
+    case 6 :
+        console.log('Hj é Sexta');
+        break
+    case 7 :
+        console.log('Hj é Sabado');
+        break
+    default :
+        console.log(`${day} não é um dia`);
+        break
+
+
+}
